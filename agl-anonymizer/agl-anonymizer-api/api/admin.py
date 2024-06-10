ab6ecf95793deac4633b1797aaf6d590936b6253
maddonix <tlux14@googlemail.com>
2024-06-10 19:33:18 +0200

    removed old version

--- agl-anonymizer/agl-anonymizer-api/api/admin.py
+++
@@ -1,7 +0,0 @@
-
-# Register your models here.
-from django.contrib import admin
-from .models import UploadedFile, AnonymizedFile
-
-admin.site.register(UploadedFile)
-admin.site.register(AnonymizedFile)
